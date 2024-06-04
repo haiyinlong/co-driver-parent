41f32b6461de6b89292aa3ee2370d95d8ca65c6a
haiyinlong <haiyinlong@uhigame.com>
2024-06-04 19:05:16 +0800

    fix: 修改打印汇率

--- co-driver-services/co-driver-service-common/src/main/java/com/leo/ad/codriver/common/ExchangeRate.java
+++ co-driver-services/co-driver-service-common/src/main/java/com/leo/ad/codriver/common/ExchangeRate.java
@@ -1,5 +1,6 @@
 package com.leo.ad.codriver.common;
 
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -29,5 +30,10 @@ public class ExchangeRate {
         }
         log.warn("获取印度转美元汇率失败,redis中没有获取到这个key:{}", "feeUSDToINR");
         return BigDecimal.ZERO;
+    }
+
+    @PostConstruct
+    public void print() {
+        log.info("获取当前汇率:{}", getIndianToDollar());
     }
 }
