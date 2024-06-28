945e8c5b253f8a4a29b36385a71538a802bb826e
haiyinlong <haiyinlong@uhigame.com>
2024-06-28 10:31:55 +0800

    fix: 修改目录结构

---
+++ co-driver-monitor/src/main/java/com/leo/ad/codrivermonitor/CoDriverMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.leo.ad.codrivermonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoDriverMonitorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoDriverMonitorApplication.class, args);
+    }
+
+}
---
+++ co-driver-monitor/src/test/java/com/leo/ad/codrivermonitor/CoDriverMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leo.ad.codrivermonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoDriverMonitorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
