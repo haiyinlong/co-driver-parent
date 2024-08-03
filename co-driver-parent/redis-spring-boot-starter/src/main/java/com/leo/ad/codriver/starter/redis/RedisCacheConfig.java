a69cdaa4fb9855c2ec9c39a9ced28a71f5eea43c
haiyinlong <haiyinlong@uhigame.com>
2024-08-03 16:49:32 +0800

    fix: 添加cache缓存配置

---
+++ co-driver-parent/redis-spring-boot-starter/src/main/java/com/leo/ad/codriver/starter/redis/RedisCacheConfig.java
@@ -0,0 +1,46 @@
+package com.leo.ad.codriver.starter.redis;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.time.Duration;
+
+/**
+ * RedisCacheConfig
+ *
+ * @author HaiYinLong
+ * @version 2024/08/03 15:51
+ **/
+@Configuration
+public class RedisCacheConfig {
+    @Bean
+    @ConditionalOnClass(RedisCacheManager.class)
+    @ConditionalOnMissingBean
+    public RedisCacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        mapper.activateDefaultTyping(mapper.getPolymorphicTypeValidator(), ObjectMapper.DefaultTyping.NON_FINAL);
+        Jackson2JsonRedisSerializer<Object> jackson2JsonRedisSerializer =
+            new Jackson2JsonRedisSerializer<>(mapper, Object.class);
+
+        RedisCacheConfiguration cacheConfiguration =
+            RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofDays(30))
+                .serializeKeysWith(
+                    RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
+                .serializeValuesWith(
+                    RedisSerializationContext.SerializationPair.fromSerializer(jackson2JsonRedisSerializer));
+
+        return RedisCacheManager.builder(redisConnectionFactory).cacheDefaults(cacheConfiguration).build();
+    }
+}
