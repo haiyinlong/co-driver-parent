9484ec49be8433bff1b1994e70ed5d3e2b277ec0
haiyinlong <haiyinlong@uhigame.com>
2024-08-05 09:51:14 +0800

    fix: 修改swagger 添加authorized 配置

--- co-driver-parent/swagger-spring-boot-starter/src/main/java/com/leo/ad/codriver/starter/swagger/SwaggerDefaultConfig.java
+++ co-driver-parent/swagger-spring-boot-starter/src/main/java/com/leo/ad/codriver/starter/swagger/SwaggerDefaultConfig.java
@@ -1,13 +1,22 @@
 package com.leo.ad.codriver.starter.swagger;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.parameters.Parameter;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.ObjectUtils;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * SwaggerConfig
@@ -17,18 +26,28 @@ import org.springframework.context.annotation.Configuration;
  **/
 @Configuration
 public class SwaggerDefaultConfig {
+    @Value("${spring.application.name}")
+    private String appName;
+
+    @Value("${co-driver.swagger.authorized.field}")
+    private String authorizedField;
     @Bean
     @ConditionalOnMissingBean(name = "swaggerOpenApi")
     public OpenAPI swaggerOpenApi() {
+
         return new OpenAPI()
-                .info(new Info().title("标题")
+                .components(new Components().addSecuritySchemes("token",
+                        new SecurityScheme().type(SecurityScheme.Type.APIKEY).in(SecurityScheme.In.HEADER).name(getAuthorizedField())))
+                .addSecurityItem(new SecurityRequirement().addList("token"))
+                .info(new Info().title(appName)
                         .contact(new Contact())
-                        .description("我的API文档")
-                        .version("v1")
-                        .license(new License().name("Apache 2.0").url("http://springdoc.org"))
-                )
-                .externalDocs(new ExternalDocumentation()
-                        .description("外部文档")
-                        .url("https://springshop.wiki.github.org/docs"));
+                        .description(appName +"API文档"));
+    }
+
+    private String getAuthorizedField() {
+        if(ObjectUtils.isEmpty(authorizedField)){
+            return "token";
+        }
+        return authorizedField;
     }
 }
