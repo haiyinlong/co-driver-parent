bf882cc1659d6f6f9d671425ad9503e188f1c5eb
chenwenjie <13392808093@163.com>
2024-09-06 18:29:16 +0800

    oeta游戏报表handler

---
+++ co-driver-statistics/co-driver-statistics-services/co-driver-statistics-service-ads/src/main/java/com/leo/ad/codriver/ads/eventHandler/AdsDailyGameOetaReportEventHandler.java
@@ -0,0 +1,35 @@
+package com.leo.ad.codriver.ads.eventHandler;
+
+import com.leo.ad.codriver.ads.service.AdsService;
+import com.leo.ad.codriver.common.event.dws.DwsDailyPackageAllGameUpdateDwEvent;
+import com.leo.ad.codriver.common.event.dws.DwsDailyPackageAllLabAdUpdateDwEvent;
+import com.leo.ad.codriver.common.event.dws.DwsDailyPackageAllLabVersionPromotionUpdateDwEvent;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
+
+/**
+ * AdsDailyLabOetaBaseReportEventHandler
+ *
+ * @author HaiYinLong
+ * @version 2024/09/04 16:03
+ **/
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class AdsDailyGameOetaReportEventHandler {
+    private final AdsService adsDailyGameOetaReportServiceImpl;
+
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+    @Async
+    public void handleEvent(DwsDailyPackageAllGameUpdateDwEvent dwsDailyPackageAllGameUpdateDwEvent) {
+        log.info("{} DwsDailyPackageAllLabAdUpdateEvent事件触发 adsDailyLabOetaBaseReport",
+                dwsDailyPackageAllGameUpdateDwEvent.getDates());
+        adsDailyGameOetaReportServiceImpl.syncData(dwsDailyPackageAllGameUpdateDwEvent.getDates());
+    }
+
+
+}
