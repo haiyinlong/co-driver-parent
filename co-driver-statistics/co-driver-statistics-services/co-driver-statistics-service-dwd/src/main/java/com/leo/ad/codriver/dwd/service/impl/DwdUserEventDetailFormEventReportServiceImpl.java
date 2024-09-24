2af10ea1e4940ddbab6d2c342a95bd1b1289d610
haiyinlong <haiyinlong@uhigame.com>
2024-09-24 09:32:28 +0800

    fix: 修改eventReport 数据同步方式

--- co-driver-statistics/co-driver-statistics-services/co-driver-statistics-service-dwd/src/main/java/com/leo/ad/codriver/dwd/service/impl/DwdUserEventDetailFormEventReportServiceImpl.java
+++ co-driver-statistics/co-driver-statistics-services/co-driver-statistics-service-dwd/src/main/java/com/leo/ad/codriver/dwd/service/impl/DwdUserEventDetailFormEventReportServiceImpl.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.ObjectUtils;
 
-import com.leo.ad.codriver.common.util.DateUtils;
 import com.leo.ad.codriver.dwd.dao.DwdUserEventDetailMapper;
 import com.leo.ad.codriver.dwd.dto.DataChangeDTO;
 import com.leo.ad.codriver.dwd.entity.DwdUserEventDetail;
@@ -33,14 +32,9 @@ public class DwdUserEventDetailFormEventReportServiceImpl implements DwdStreamSe
     @Override
     @Transactional(rollbackFor = Exception.class)
     public boolean syncChangeData(DataChangeDTO dataChangeDTO) {
-        // 只处理7天内的数据，其他数据直接丢弃
-        if (dataChangeDTO.getDates() < DateUtils.getPreviousDate(7)) {
-            return true;
-        }
         DwdUserEventDetail dwdUserEventDetail =
             dwdUserEventDetailMapper.getStatisticsEventReport(ODS_EVENT_REPORT, dataChangeDTO.getSourceId());
         if (ObjectUtils.isEmpty(dwdUserEventDetail)) {
-            dwdUserEventDetailMapper.deleteEventReportBySourceId(ODS_EVENT_REPORT, dataChangeDTO.getSourceId());
             return true;
         }
         batchMapper.batchInsert(Collections.singletonList(dwdUserEventDetail), DwdUserEventDetailMapper.class);
