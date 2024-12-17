0a66bffe70e16d0e75704aa9501e9df143f56142
haiyinlong <haiyinlong@uhigame.com>
2024-12-17 17:24:01 +0800

    fix: 修改日志打印

--- co-driver-statistics/co-driver-statistics-services/co-driver-statistics-service-dim/src/main/java/com/leo/ad/codriver/dim/service/impl/DimGameUserInfoServiceImpl.java
+++ co-driver-statistics/co-driver-statistics-services/co-driver-statistics-service-dim/src/main/java/com/leo/ad/codriver/dim/service/impl/DimGameUserInfoServiceImpl.java
@@ -54,7 +54,7 @@ public class DimGameUserInfoServiceImpl implements DimGameUserInfoService {
             dimGameUserInfo.modifyUpdateTime();
             dimGameUserInfoMapper.updateById(dimGameUserInfo);
         } catch (Exception e) {
-            log.error("userId:" + gameUserId + " syncGameUser error, lockKey:" + lockKey, e);
+            log.error("ods_game_user_one_net.id:" + gameUserId + " syncGameUser error, lockKey:" + lockKey, e);
             throw new RuntimeException(e);
         } finally {
             if (rLock.isLocked()) {
