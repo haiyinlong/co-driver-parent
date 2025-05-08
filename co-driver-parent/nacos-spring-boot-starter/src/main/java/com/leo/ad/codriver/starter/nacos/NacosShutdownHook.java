69b8526dd0441c7248eb76636b995bd410b45a38
haiyinlong <haiyinlong@uhigame.com>
2025-05-08 10:31:24 +0800

    fix: nacos添加排除操作

--- co-driver-parent/nacos-spring-boot-starter/src/main/java/com/leo/ad/codriver/starter/nacos/NacosShutdownHook.java
+++ co-driver-parent/nacos-spring-boot-starter/src/main/java/com/leo/ad/codriver/starter/nacos/NacosShutdownHook.java
@@ -1,11 +1,14 @@
 package com.leo.ad.codriver.starter.nacos;
 
-import com.alibaba.cloud.nacos.registry.NacosRegistration;
-import com.alibaba.cloud.nacos.registry.NacosServiceRegistry;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
+
+import com.alibaba.cloud.nacos.registry.NacosRegistration;
+import com.alibaba.cloud.nacos.registry.NacosServiceRegistry;
+
+import lombok.RequiredArgsConstructor;
 
 /**
  * NacosShutdownHook
@@ -16,6 +19,7 @@ import org.springframework.stereotype.Component;
 @Configuration
 @Component
 @RequiredArgsConstructor
+@Profile("!prod-test")
 public class NacosShutdownHook implements DisposableBean {
     private final NacosServiceRegistry nacosServiceRegistry;
     private final NacosRegistration nacosRegistration;
