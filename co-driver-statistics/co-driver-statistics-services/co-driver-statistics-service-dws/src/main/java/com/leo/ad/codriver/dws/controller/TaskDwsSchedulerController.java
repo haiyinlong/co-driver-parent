ce59972d47e5d85e9c5144a16c3214cb344a8593
haiyinlong <haiyinlong@uhigame.com>
2025-07-01 11:13:15 +0800

    fix: 添加dws层抛出事件方法

---
+++ co-driver-statistics/co-driver-statistics-services/co-driver-statistics-service-dws/src/main/java/com/leo/ad/codriver/dws/controller/TaskDwsSchedulerController.java
@@ -0,0 +1,41 @@
+package com.leo.ad.codriver.dws.controller;
+
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.util.ObjectUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.leo.ad.codriver.common.util.DateUtils;
+import com.leo.ad.codriver.dws.event.DwsUpdateFinishEvent;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * DwController
+ *
+ * @author HaiYinLong
+ * @version 2024/04/16
+ **/
+@RestController
+@RequestMapping("/scheduler/dws")
+@Tag(name = "DwsController控制层", description = "Dws层任务")
+@AllArgsConstructor
+@Slf4j
+public class TaskDwsSchedulerController {
+    private final ApplicationEventPublisher publisher;
+
+    @GetMapping("/pushEvent/{dates}")
+    @Operation(summary = "触发所有dwd", description = "发送dwdUpdateFinishEvent")
+    public String dwdHandle(@PathVariable("dates") Integer dates) {
+        if (ObjectUtils.isEmpty(dates)) {
+            dates = DateUtils.getPreviousDate();
+        }
+        publisher.publishEvent(new DwsUpdateFinishEvent(this, dates));
+        return "执行完成dwd数据同步";
+    }
+}
