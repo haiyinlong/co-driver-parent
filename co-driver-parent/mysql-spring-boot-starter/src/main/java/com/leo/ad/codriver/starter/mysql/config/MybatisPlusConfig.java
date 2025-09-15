a97a309e85aef3a07d0c39b55f3662a230ac9882
haiyinlong <haiyinlong@uhigame.com>
2025-09-15 11:18:33 +0800

    fix: mysql 添加配置

--- co-driver-parent/mysql-spring-boot-starter/src/main/java/com/leo/ad/codriver/starter/mysql/config/MybatisPlusConfig.java
+++ co-driver-parent/mysql-spring-boot-starter/src/main/java/com/leo/ad/codriver/starter/mysql/config/MybatisPlusConfig.java
@@ -1,11 +1,14 @@
 package com.leo.ad.codriver.starter.mysql.config;
 
-import com.baomidou.mybatisplus.annotation.DbType;
-import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
-import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.BlockAttackInnerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.OptimisticLockerInnerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 
 /**
  * MybatisPlusConfig
@@ -20,6 +23,10 @@ public class MybatisPlusConfig {
     public MybatisPlusInterceptor mybatisPlusInterceptor() {
         MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
         interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        // 设置乐观锁
+        interceptor.addInnerInterceptor(new OptimisticLockerInnerInterceptor());
+        // 防止全表操作,更新和删除
+        interceptor.addInnerInterceptor(new BlockAttackInnerInterceptor());
         return interceptor;
     }
 }
