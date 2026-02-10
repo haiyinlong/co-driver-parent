e800c90d4b0d9f4067dee4ccc6da8b2590dc437e
haiyinlong <haiyinlong@yeah.com>
2026-02-10 17:36:48 +0800

    fix: 优化实时计算当天用户数

--- co-driver-statistics/co-driver-statistics-services/co-driver-statistics-service-dws/src/main/java/com/leo/ad/codriver/dws/controller/TaskDwsSchedulerController.java
+++ co-driver-statistics/co-driver-statistics-services/co-driver-statistics-service-dws/src/main/java/com/leo/ad/codriver/dws/controller/TaskDwsSchedulerController.java
@@ -1,19 +1,18 @@
 package com.leo.ad.codriver.dws.controller;
 
+import com.leo.ad.codriver.common.util.DateUtils;
+import com.leo.ad.codriver.dws.event.DwsUpdateFinishEvent;
+import com.leo.ad.codriver.dws.scheduler.DwsTodayRealTimeScheduler;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.leo.ad.codriver.common.util.DateUtils;
-import com.leo.ad.codriver.dws.event.DwsUpdateFinishEvent;
-
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
-import lombok.AllArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 
 /**
  * DwController
@@ -28,14 +27,22 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class TaskDwsSchedulerController {
     private final ApplicationEventPublisher publisher;
+    private final DwsTodayRealTimeScheduler dwsTodayRealTimeScheduler;
 
     @GetMapping("/pushEvent/{dates}")
-    @Operation(summary = "触发所有dwd", description = "发送dwdUpdateFinishEvent")
+    @Operation(summary = "触发所有dws", description = "发送dwsUpdateFinishEvent")
     public String dwdHandle(@PathVariable("dates") Integer dates) {
         if (ObjectUtils.isEmpty(dates)) {
             dates = DateUtils.getPreviousDate();
         }
         publisher.publishEvent(new DwsUpdateFinishEvent(this, dates));
-        return "执行完成dwd数据同步";
+        return "执行完成dws数据同步";
+    }
+
+    @GetMapping("/currentUser")
+    @Operation(summary = "TriggerDwsUser", description = "触发DwsUser统计")
+    public String currentUser() {
+        dwsTodayRealTimeScheduler.triggerUserCalculat();
+        return "执行完成DwsUser统计";
     }
 }
--- co-driver-statistics/co-driver-statistics-services/co-driver-statistics-service-dws/src/main/java/com/leo/ad/codriver/dws/scheduler/DwsTodayRealTimeScheduler.java
+++ co-driver-statistics/co-driver-statistics-services/co-driver-statistics-service-dws/src/main/java/com/leo/ad/codriver/dws/scheduler/DwsTodayRealTimeScheduler.java
@@ -18,22 +18,54 @@ import org.springframework.stereotype.Component;
 @Slf4j
 @RequiredArgsConstructor
 public class DwsTodayRealTimeScheduler {
+    private final DwsService dwsDailyPackageAllLabLoginServiceImpl;
+    private final DwsService dwsDailyPackageLoginServiceImpl;
     private final DwsService dwsDailyPackageAllLoginServiceImpl;
+    private final DwsService dwsDailyPkgLoginServiceImpl;
+    private final DwsService dwsDailyPkgUsrcLoginServiceImpl;
+    private final DwsService dwsDailyPkgVerUsrcLoginServiceImpl;
 
     /**
      * 实时更新当天数据，每小时更新一次当天的历史数据，每天晚上同一再处理一次保证数据的真确性<br/>
      */
     @Scheduled(cron = "0 0 1-22 * * ?")
     @Async("asyncServiceExecutor")
-    public void updateLoginDate() {
+    public void triggerUserCalculat() {
         Integer dates = DateUtils.getNowDates();
-        log.info("{} dwsDailyPackageAllLoginServiceImpl 开始实时同步所有数据", dates);
+        log.info("{} dws 用户实时数据同步所有数据", dates);
+        try {
+            dwsDailyPackageAllLabLoginServiceImpl.syncData(dates);
+        } catch (Exception e) {
+            log.error(
+                dates + "当天" + dwsDailyPackageAllLabLoginServiceImpl.getClass().getSimpleName() + " 数据同步异常", e);
+        }
+        try {
+            dwsDailyPackageLoginServiceImpl.syncData(dates);
+        } catch (Exception e) {
+            log.error(dates + "当天" + dwsDailyPackageLoginServiceImpl.getClass().getSimpleName() + " 数据同步异常", e);
+        }
         try {
             dwsDailyPackageAllLoginServiceImpl.syncData(dates);
         } catch (Exception e) {
             log.error(dates + "当天" + dwsDailyPackageAllLoginServiceImpl.getClass().getSimpleName() + " 数据同步异常",
                 e);
         }
-        log.info("{} dwsDailyPackageAllLoginServiceImpl 实时同步所有数据同步结束", dates);
+        try {
+            dwsDailyPkgLoginServiceImpl.syncData(dates);
+        } catch (Exception e) {
+            log.error(dates + "当天" + dwsDailyPkgLoginServiceImpl.getClass().getSimpleName() + " 数据同步异常", e);
+        }
+        try {
+            dwsDailyPkgUsrcLoginServiceImpl.syncData(dates);
+        } catch (Exception e) {
+            log.error(dates + "当天" + dwsDailyPkgUsrcLoginServiceImpl.getClass().getSimpleName() + " 数据同步异常", e);
+        }
+        try {
+            dwsDailyPkgVerUsrcLoginServiceImpl.syncData(dates);
+        } catch (Exception e) {
+            log.error(dates + "当天" + dwsDailyPkgVerUsrcLoginServiceImpl.getClass().getSimpleName() + " 数据同步异常",
+                e);
+        }
+        log.info("{} 用户实时数据同步结束", dates);
     }
 }
